07dd20a7ae27e6c6ef32abfab318da34410df825
Apollo-111 <joem10067@gmail.com>
2022-05-18 19:06:15 -0400

    improved

--- main/src/js/MVC/views/header/Header.js
+++ main/src/js/MVC/views/header/Header.js
@@ -1,25 +1,51 @@
 import Views from "../Views";
 
 class Header extends Views {
-  //remove thee following after test
-  // Makes header fixed
   hamburgerAndXContainer = document.querySelector(".hamburger-x__container");
-
-  //
   header = document.querySelector("header");
+  navElement = document.querySelector("nav");
+  navChildren = document.querySelectorAll(".nav-lbl");
 
   //
   addHandler(handler) {
     this.hamburgerAndXContainer.addEventListener("click", (e) => {
       this.toggleFixedHeader(e);
     });
+
+    console.log(this.navChildren);
+
+    this.navChildren.forEach((child) =>
+      child.addEventListener("mouseenter", (e) => {
+        this.navOnMouseEnter(e);
+      })
+    );
   }
 
+  // Makes header fixed
   toggleFixedHeader(e) {
-    console.log(this.header);
-
-    this.main.style;
+    this.main.classList.toggle("for-main__header-active");
     this.header.classList.toggle("dt-fixed-header");
+  }
+
+  // Controls nav hovering
+  navOnMouseEnter(e) {
+    // Add styles to all children elements of nav
+    this.navChildren.forEach((child) => {
+      child.style.opacity = "0.2";
+      child.style.filter = "blur(2px)";
+    });
+
+    // Remove styles on target element
+    e.target.style.opacity = "1";
+    e.target.style.filter = "none";
+
+    this.navElement.addEventListener("mouseleave", () => {
+      this.navChildren.forEach((child) => {
+        // Remove all styles when mouse leaves nav bar
+        child.style.opacity = "1";
+        child.style.filter = "none";
+      });
+    });
   }
 }
 
