07dc3e8a9f647bf1034d6ae8b940cd8fe78901f3
Apollo-111 <joem10067@gmail.com>
2022-06-01 22:43:30 -0400

    added section 4 with initial logic

--- main/src/js/MVC/controller.js
+++ main/src/js/MVC/controller.js
@@ -1,26 +1,26 @@
 import { lightTheme } from "./model.js"; // state.save, state.load
-import lightThemeView from "./views/header/LightThemeView.js";
+import LightThemeView from "./views/header/LightThemeView.js";
 import Header from "./views/header/Header.js";
 import MenuView from "./views/header/MenuView.js";
 import Typewriter from "./views/Typewriter.js";
-import toolbox from "./views/Toolbox.js";
+import Slider from "./views/Slider.js";
 
 const controlTheme = function () {
   lightTheme.isActive = !lightTheme.isActive;
   lightTheme.isActive ? lightTheme.save() : lightTheme.delete();
-  lightThemeView.enableLightTheme(lightTheme.isActive);
+  LightThemeView.enableLightTheme(lightTheme.isActive);
 };
 
 const init = function () {
   if (lightTheme.load()) {
     lightTheme.isActive = !lightTheme.isActive;
-    lightThemeView.enableLightTheme(lightTheme.isActive).toggleSwitch();
+    LightThemeView.enableLightTheme(lightTheme.isActive).toggleSwitch();
   }
-  lightThemeView.addHandler(controlTheme);
+  LightThemeView.addHandler(controlTheme);
   MenuView.addHandler();
   Header.addHandler();
   Typewriter.addHandler();
-  // toolbox.addHandler();
+  Slider.addHandler();
 };
 
 init();
