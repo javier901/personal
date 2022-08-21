6093326ba603f252ccb797dcba37e699c182c9c8
Apollo-111 <joem10067@gmail.com>
2022-08-21 19:14:15 -0400

    set section intersection threshold

--- main/src/js/mvc/controller.js
+++ main/src/js/mvc/controller.js
@@ -68,7 +68,7 @@ const controlFormSubmission = async function (formData) {
 const controlIntersections = function (elementsToObserve, behaviorCallback) {
   const options = {
     root: null,
-    threshold: 0.03,
+    threshold: 0.1,
   };
   const observer = new IntersectionObserver(behaviorCallback, options);
 
